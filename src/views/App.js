b1f52ae1c4e71e25430096692982d69145e55032
thunguyen <thu.nguyen@tomosia.com>
2022-12-27 11:34:06 +0700

    feature react basic

--- src/views/App.js
+++ src/views/App.js
@@ -1,30 +1,52 @@
-import './App.scss';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import ListTodoComponent from './Todos/ListTodoComponent';
-
+import "./App.scss";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ListTodoComponent from "./Todos/ListTodoComponent";
+import MyComponent from "./Example/MyComponent";
+import NavComponent from "./Nav/NavComponent";
+import HomeComponent from "./Example/HomeComponent";
+import ListUser from "./Users/ListUser";
+import DetailUser from "./Users/DetailUser";
+import { BrowserRouter, Switch } from "react-router-dom";
+import { Route } from "react-router";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Simple TODO Apps with React.js
-        </p>
-        <ListTodoComponent/>
-      </header>
-      <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="colored"
-      />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <NavComponent />
+          <Switch>
+            <Route path="/" exact>
+              <HomeComponent />
+            </Route>
+            <Route path="/todo">
+              <ListTodoComponent />
+            </Route>
+            <Route path="/about">
+              <MyComponent />
+            </Route>
+            <Route path="/user" exact>
+              <ListUser />
+            </Route>
+            <Route path="/user/:id">
+              <DetailUser />
+            </Route>
+          </Switch>
+        </header>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
